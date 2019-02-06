accba451d112082d31a3a89d554c0a6f03e6f155
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-06 23:20:23 +0100

    Add project files.

---
+++ ConsoleAppDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
